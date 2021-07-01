efe4cd8796fad77ef58bde80154c33235468a849
Suman Kumar <suman@Sumans-MacBook-Pro-2.local>
2021-07-02 00:07:18 +0530

    WIP Search app intial commit

---
+++ pages/about.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function About (){
+    return(
+    
+         <div className="container">
+            <Head>
+              <title>Create Next App</title>
+              <link rel="icon" href="/favicon.ico" />
+              <link rel="stylesheet" href="/styles.css"/>
+            </Head>
+          <h1>About</h1>
+        </div>
+    )
+}
